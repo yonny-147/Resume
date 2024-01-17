0cc6acc8e2964a5345d9a7da7f6084450c7b0696
YonnyDev <[yonny-147@hotmailcom]>
2024-01-17 08:37:03 -0500

    Resume v4.6

--- src/components/BarNavegation.jsx
+++ src/components/BarNavegation.jsx
@@ -1,27 +1,39 @@
 import { Link } from 'react-scroll';
 import '../styles/index.css'
+import { useState } from 'react';
+
 export const BarNavegation = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const fMenuOpen = () => {
+        setMenuOpen(true)
+    }
+
+    const fMenuClose = () => {
+        setMenuOpen(false)
+    }
 
   return (
     <>
-    <div className='header'>
-        <img className='cancel' src="./imgs/cancelar.png" alt="" />
+    <div className={`header ${menuOpen ? "open" : ""}`}>
+        <img className='cancel' src="./imgs/cancelar.png" alt="cancelar" onClick={fMenuClose}/>
         <nav>
             <ul>
                 <li>
-                    <Link to='home' smooth={true} duration={500} className='firsText' >About</Link>
+                    <Link to='home' smooth={true} duration={500} className='firsText' onClick={fMenuClose} >About</Link>
                 </li>
                 <li>
-                    <Link to='projects' smooth={true} duration={500} className='firsText' >Projects</Link>
+                    <Link to='projects' smooth={true} duration={500} className='firsText' onClick={fMenuClose}>Projects</Link>
                 </li>
                 <li>
-                    <Link to='contact' smooth={true} duration={500} className='firsText' >Contact</Link>
+                    <Link to='contact' smooth={true} duration={500} className='firsText' onClick={fMenuClose}>Contact</Link>
                 </li>
             </ul>
         </nav>
     </div>
     <div>
-        <img className='menu' src="./imgs/menu.png" alt="menu" />
+        <img className='menu' src="./imgs/menu.png" alt="menu" onClick={fMenuOpen}/>
     </div>
     </>
   )
